5257f40c451a3eb6c7b02ba2a147cf4ac08c981b
stellachy <chy.stella.chuang@gmail.com>
2025-02-04 20:43:24 +0800

    調整css

---
+++ index.js
@@ -0,0 +1,10 @@
+// 點擊[＋品種]產生新的一列
+const addVarietyBtn = document.getElementById('addVarietyBtn');
+
+addVarietyBtn.addEventListener('click', showVarRow);
+function showVarRow () {
+  console.log(567);
+  // 取得訂購內容的table
+
+  // 找到下一行
+}
