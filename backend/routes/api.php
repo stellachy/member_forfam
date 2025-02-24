5660137738abdfa3a6818569d53e5d9c40f7f376
stellachy <chy.stella.chuang@gmail.com>
2025-02-24 11:28:03 +0800

    fix .yml related to git commit，測試部署到ec2 8th

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,6 +15,6 @@ Route::post('/c', [CustomersController::class, 'addCnO']);
 Route::post('/o', [OrdersController::class, 'addOrder']);
 Route::get('/c/check', [CustomersController::class, 'searchC']);
 Route::get('/c/{id}', [CustomersController::class, 'searchCinfo']);
-Route::get('/e', function () {
+Route::get('/f', function () {
     return Customers::all();
 });
