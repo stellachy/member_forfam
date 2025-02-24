09969030fc8a085e0b827c04f44ebbb2eb9cc71d
stellachy <chy.stella.chuang@gmail.com>
2025-02-24 10:49:00 +0800

    fix remote side & try shortening .yml，測試部署到ec2 7th

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,6 +15,6 @@ Route::post('/c', [CustomersController::class, 'addCnO']);
 Route::post('/o', [OrdersController::class, 'addOrder']);
 Route::get('/c/check', [CustomersController::class, 'searchC']);
 Route::get('/c/{id}', [CustomersController::class, 'searchCinfo']);
-Route::get('/d', function () {
+Route::get('/e', function () {
     return Customers::all();
 });
